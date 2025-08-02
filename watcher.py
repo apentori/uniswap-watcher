9379829a4d35bfb88942395c5548aab6f123b6fd
apentori <pentori.alexis@proton.me>
2025-08-02 14:18:22 +0200

    v0.1: postgresl export   * add args   * add dockerfile and docker-compose
    Signed-off-by: apentori <pentori.alexis@proton.me>

--- watcher.py
+++ watcher.py
@@ -2,14 +2,18 @@
 import os
 import logging
 import json
+import yaml
 import csv
+import click
+import psycopg2
 from web3 import Web3
 from uniswap import Uniswap
 from time import gmtime, strftime, sleep
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 
-PROVIDER_URL = os.environ.get('INFURA_BASE_URL','https://mainnet.infura.io/v3/') + os.environ.get('INFURA_API_KEY')
+INFURA_BASE_URL="https://mainnet.infura.io/v3"
 FILE_NAME="price.csv"
 class Price:
     def __init__(self, token_1, token_2, amount, time):
@@ -27,12 +31,12 @@ class Price:
         }
 
 class UniswapClient:
-    def __init__(self):
+    def __init__(self, api_key):
         self.client = Uniswap(
             address=None,
             private_key=None,
             version=3,
-            provider=PROVIDER_URL
+            provider=f"{INFURA_BASE_URL}/{api_key}"
         )
 
     def get_token_price(self, token_1, token_2):
@@ -40,6 +44,27 @@ class UniswapClient:
         logging.debug("The Token price is %s", price)
         p = Price(token_1.get("name"), token_2.get("name"), price / 10**18, strftime("%Y%m%dT%H:%M:%S", gmtime()))
         return p
+
+class Postgres:
+    def __init__(self,host, port, name, user, password):
+        self.conn = psycopg2.connect(database=name, host=host, user=user, password=password, port=port)
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS prices (token_1 TEXT, token_2 TEXT, amount float, update_on TIMESTAMP);")
+        self.conn.commit()
+        cursor.close()
+
+    def insert(self, price):
+        cursor = self.conn.cursor()
+        logging.debug("Insert Price %s in the database", price)
+        datetime_price = datetime.strptime(price.time, "%Y%m%dT%H:%M:%S")
+        cursor.execute(f"INSERT INTO prices (token_1, token_2, amount, update_on) VALUES ('{price.token_1}', '{price.token_2}', {price.amount}, '{price.time}')")
+        self.conn.commit()
+        cursor.close()
+
+
+    def close(self):
+        self.conn.close()
+
 
 SNT={"name": "SNT", "address": "0x744d70FDBE2Ba4CF95131626614a1763DF805B9E" }
 ETH={"name": "ETH", "address": "0x0000000000000000000000000000000000000000"}
@@ -50,13 +75,25 @@ def save_price(price):
         logging.info(price.to_dict())
         writer.writerow(price.to_dict())
 
-def main():
-    client = UniswapClient()
+@click.command()
+@click.option("-k", "--api-key",        prompt="Enter Infura API Key",          help="Infura API Key")
+@click.option("-f", "--frequency",      default=300,                            help="Frequency between each call (in sec)")
+@click.option("-h", "--psql-host",      default="localhost",                    help="Postgres Host")
+@click.option("-p", "--psql-port",      default=5432,                           help="Postgres Port")
+@click.option("-d", "--psql-database",  default="postgres",                     help="Postgres Database")
+@click.option("-u", "--psql-username",  default="postgres",                      help="Postgres User")
+@click.option("-P", "--psql-password",  prompt="Postgres Password",             help="Postgres Password")
+@click.option("-l", "--log-level",      default="INFO",                         help="Log Level")
+def main(api_key, frequency, psql_host, psql_port, psql_database, psql_username, psql_password, log_level):
+    client = UniswapClient(api_key)
+    psqlClient = Postgres(psql_host, psql_port, psql_database, psql_username, psql_password)
     while True:
         p = client.get_token_price(ETH, SNT)
         logging.info("The 1 eth is worth : %s SNT", p.amount)
-        save_price(p)
-        sleep(60)
+
+        #save_price(p)
+        psqlClient.insert(p)
+        sleep(frequency)
 
 main()
 
